92a34d37096421ac172cefef8a8b6a937cc91bec
xavier0509 <xiejr_work@163.com>
2016-12-06 11:30:01 +0800

    modify:the function of getip && sql

--- php/getipaddr.php
+++ php/getipaddr.php
@@ -1,13 +1,18 @@
 <?php 
 
+$conn = mysql_connect("127.0.0.1","root","000000")or die ("连接数据库服务器失败！".mysql_error());
+$select = mysql_select_db("tvdoctor",$conn);
+
 $ipID = "1";
-$sql = "select ipAddr from ipTable where ipId='" . $ipID . "'";
+$sql = "select * from ipTable where ipId='" . $ipID . "'";
 $result = mysql_query($sql);
 $list = mysql_fetch_assoc($result);
 $getip = $list['ipAddr'];
+$getport = $list['port'];
 if ($list != "") {
     # code...
     echo $getip;
+    echo '{"ret": "OK", "data": {"ipAddr":"'.$getip.'","port":"'.$getport.'"}}';
 }
 else{
     echo "null";
--- sql/creatDatabase.sql
+++ sql/creatDatabase.sql
@@ -150,11 +150,12 @@ DROP TABLE IF EXISTS `ipTable`;
 CREATE TABLE `ipTable` (
   `ipId` int(5) NOT NULL,
   `ipAddr` char(20) NOT NULL,
+  `port` int(5) NOT NULL,
   PRIMARY KEY (`ipId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of ipTable
 -- ----------------------------
-INSERT INTO `ipTable` VALUES ('1', '120.27.147.96');
+INSERT INTO `ipTable` VALUES ('1', '120.27.147.96','9002');
 
