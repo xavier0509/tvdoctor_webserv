97e5e0caf9586b075ae3bd74252026f03bb3575c
xavier <xiejr_work@163.com>
2019-04-22 18:01:57 +0800

    恢复php版本

--- php/login.php
+++ php/login.php
@@ -1,13 +1,13 @@
 <?php 
 @session_start();
-$conn = mysqli_connect("223.202.11.125","root","000000")or die ("连接数据库服务器失败！".mysqli_error());
-$select = mysqli_select_db("tvdoctor",$conn);
+$conn = mysql_connect("223.202.11.125","root","000000")or die ("连接数据库服务器失败！".mysql_error());
+$select = mysql_select_db("tvdoctor",$conn);
 
 $user = $_GET['username1'];
 $pw = $_GET['password1'];
 $sql = "select password from user where userName='" . $user . "'";
-$result = mysqli_query($sql);
-$list = mysqli_fetch_assoc($result);
+$result = mysql_query($sql);
+$list = mysql_fetch_assoc($result);
 $getpw = $list['password'];
 //$getname=$list['userName'];
 //$getlevel=$list[level];
