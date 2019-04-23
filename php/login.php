0a22cccf5e746e51317253d2e9e5afd05936a138
xavier <xiejr_work@163.com>
2019-04-23 14:45:59 +0800

    本地数据库

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php 
 @session_start();
-$conn = mysql_connect("223.202.11.125","root","000000")or die ("连接数据库服务器失败！".mysql_error());
+$conn = mysql_connect("127.0.0.1","root","000000")or die ("连接数据库服务器失败！".mysql_error());
 $select = mysql_select_db("tvdoctor",$conn);
 
 $user = $_GET['username1'];
